783d30af8a0d99460b922f1aacd22c02610afd71
Lars <lars.schloemer1@gmail.com>
2017-09-27 15:41:30 +0200

    Remove non core scheduler from demo project

--- Injector/Scheduler/TaskLib.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TASK_LIB_H_
-#define _TASK_LIB_H_
-//includes
-#include <ThreadingServices/ThreadPool.h>
-#include <ThreadingServices/TaskContext.h>
-#include <ThreadingServices/TaskHandle.h>
-
-#endif
