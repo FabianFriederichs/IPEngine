6c367f96bb1a6d5e6e416e1b1e5cc781609c7245
Fabian Friederichs <fabian.friederichs@gmail.com>
2018-02-25 14:12:46 +0100

    added some missing core files

---
+++ IPDefinitiveVersion/Core/IPCore/DebugMonitoring/DebugMonitor.cpp
@@ -0,0 +1,1 @@
+#include "DebugMonitor.h"
---
+++ IPDefinitiveVersion/Core/IPCore/DebugMonitoring/ErrorHandler.h
@@ -0,0 +1,11 @@
+#ifndef _ERROR_HANDLER_H_
+#define _ERROR_HANDLER_H_
+#include <IPCore/core_config.h>
+namespace ipengine
+{
+	class CORE_API DebugMonitor
+	{
+
+	};
+}
+#endif
