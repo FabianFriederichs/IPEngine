658b0957c961981ba70744175ef6e5b70c430f3b
Fabian Friederichs <fabian.friederichs@gmail.com>
2018-06-10 18:38:01 +0200

    added option to build sdl2 on the fly

--- CMake project template/root/external_dependencies/IPExternalDependencies.cmake
+++ CMake project template/root/external_dependencies/IPExternalDependencies.cmake
@@ -22,8 +22,14 @@ add_subdirectory(external_dependencies/glew) # target: glew::glew
 # vulkan sould be integrated in current cmale installs
 find_package(Vulkan) # target: Vulkan::Vulkan
 
-# sdl2 set SDL2_DIR variable to tell cmake the config path
-find_package(SDL2 REQUIRED) # target: SDL2::SDL2
+# sdl2, option 1: use a build present on the system: set SDL2_DIR variable to tell cmake the config path
+#find_package(SDL2 REQUIRED) # target: SDL2::SDL2
+
+# sdl2, option 2: build sdl2 along the project
+add_subdirectory(external_dependencies/SDL2) # target: SDL2
+if(NOT TARGET SDL2::SDL2 AND TARGET SDL2)
+add_library(SDL2::SDL2 ALIAS SDL2) # for consistency with install output
+endif()
 
 # openvr
 set(OpenVR_ROOT "${CMAKE_SOURCE_DIR}/external_dependencies/openvr")
